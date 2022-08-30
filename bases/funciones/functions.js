d9bd89fcfc6def0d8f70b9dec427e8615dde72cc
José Antonio González Morente <jgonzalezmorente@gmail.com>
2022-08-30 17:45:21 +0200

    Funciones, parámetros opciones y requeridos

---
+++ bases/funciones/functions.js
@@ -0,0 +1,12 @@
+"use strict";
+(function () {
+    var hero = 'Flash';
+    function returnName() {
+        return hero;
+    }
+    var activateBatisignal = function () {
+        return 'Batiseñal activada';
+    };
+    console.log(typeof (activateBatisignal));
+    var heroName = returnName();
+})();
