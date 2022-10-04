26098264f3af5b13757689203e8077730e035c1a
José Antonio González Morente <jgonzalezmorente@gmail.com>
2022-10-04 17:08:32 +0200

    Características ES6

---
+++ bases/ES6/let-var.ts
@@ -0,0 +1,13 @@
+(() => {
+
+    const nombre: string = 'Fernando';
+    
+    const getName = (): void => {
+        console.log( 'Viejo getName');
+    }
+
+    // getName = () => { console.log('Nuevo getName' ) }
+
+    //  getName();
+
+})();
--- bases/tipos/null-undefined.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-(function () {
-    // strictNullChecks
-    // let isActive: boolean = undefined;
-    // console.log(nada);
-})();
